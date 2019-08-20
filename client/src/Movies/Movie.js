617c7a605eea46576cd9c26f337f3cff9480ee6c
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-20 16:15:46 -0400

    added code to files and a onclick button ig uess

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SavedList from "./SavedList";
 
-const Movie = (props) => {
+const Movie = props => {
   const [movie, setMovie] = useState({});
- 
+
   useEffect(() => {
-    const id = 1;
+    const id = props.movies.find(id => props.match.params.id === `${movie.id}`);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [props.movies, movie.id, props.match.params.id]);
 
-  },[]);
-  
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -49,9 +49,9 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button">onClick={SavedList}>Save</div>
     </div>
   );
-}
+};
 
 export default Movie;

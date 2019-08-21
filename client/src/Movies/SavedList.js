53f2d86432832c4e1ab97d0843bf8ac238c19c7b
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-21 10:24:36 -0400

    updated code

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,31 @@
 import React from "react";
 
+import { NavLink, Link } from "react-router-dom";
+
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <NavLink
+        to={`/movies/${movie.id}`}
+        className="saved-movie"
+        activeClassName="active-link"
+      >
+        {movie.title}
+      </NavLink>
     ))}
-    <div className="home-button">Home</div>
+    <div style={{ display: "flex" }}>
+      <Link to="/">
+        <div className="home-button" style={{ marginRight: "5px" }}>
+          Home
+        </div>
+      </Link>
+      <div>
+        <div onClick={props.clearSavedList} className="home-button">
+          Clear
+        </div>
+      </div>
+    </div>
   </div>
 );
 
